5dfcbe87699ea0402b76dec03e47c112bb9fd905
KacperSynator <kacper.synator@gmail.com>
2023-10-24 00:07:20 +0200

    add loops and accuracy table

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use knapsack_genetic::selection_method::SelectionMethod;
 use knapsack_genetic::utils::{plot_graph, GraphData};
 use log::{error, info};
 
-fn run_and_plot_genetic_algorithm(data: &GeneticAlgorithmData<i32>, graph_data: &GraphData) {
+fn run_and_plot_genetic_algorithm(data: &GeneticAlgorithmData<i32>, graph_data: &GraphData, n_optimal_results: &mut i32) {
     match genetic_algorithm(data) {
         Ok(result) => {
-            info!("Best chromosome: {:?}", result.best_individual);
+            if result.best_individual.fitness_score == 309 { *n_optimal_results += 1 };
+            // let cr_mr = format!("cr: {}, mr: {}",data.crossover_rate, data.mutation_rate);
+            // info!("{cr_mr} => Best chromosome: {:?}", result.best_individual);
             if let Err(e) = plot_graph(&result, graph_data) {
                 error!("Failed to plot with error: {e}");
             }
@@ -18,26 +20,53 @@ fn run_and_plot_genetic_algorithm(data: &GeneticAlgorithmData<i32>, graph_data: 
     }
 }
 
+// Other example optimal 48
+// weights: vec![27, 10, 25, 25, 7],
+// prices: vec![13, 19, 7, 16, 3],
+// capacity: 66,
+
 fn main() {
     pretty_env_logger::init();
 
-    let data = GeneticAlgorithmData {
-        weights: vec![27, 10, 25, 25, 7],
-        prices: vec![13, 19, 7, 16, 3],
-        capacity: 66,
-        population_size: 50,
-        generations: 10,
-        crossover_method: CrossoverMethod::SinglePoint,
-        crossover_rate: 0.5,
-        mutation_method: MutationMethod::BitFlip,
-        mutation_rate: 0.05,
-        selection_method: SelectionMethod::Tournament { size: 10 },
-    };
+    let optimal = 309;
+    let n_reps = 100;
+    let mut accuracy_table = String::from("cr\\mr =>\n");
 
-    let graph_data = GraphData {
-        title: "GA 1",
-        ..Default::default()
-    };
-
-    run_and_plot_genetic_algorithm(&data, &graph_data);
+    for crossover_rate in [0.1, 0.3, 0.5, 0.7, 0.9] {
+        for mutation_rate in [0.1, 0.3, 0.5, 0.7, 0.9] {
+            let mut n_optimal_results = 0; 
+            for _ in 0..n_reps {
+                let data = GeneticAlgorithmData {
+                    weights: vec![23, 31, 29, 44, 53, 38, 63, 85, 89, 82],
+                    prices: vec![92, 57, 49, 68, 60, 43, 67, 84, 87, 72],
+                    capacity: 165, // optimal 309
+                    population_size: 50,
+                    generations: 50,
+                    crossover_method: CrossoverMethod::SinglePoint,
+                    crossover_rate: crossover_rate,
+                    mutation_method: MutationMethod::BitFlip,
+                    mutation_rate: mutation_rate,
+                    selection_method: SelectionMethod::Tournament { size: 5 },
+                };
+    
+                let out_file = format!("graphs/cr{crossover_rate}mr{mutation_rate}.png");
+                let title = format!("Cross={crossover_rate}, Mut={mutation_rate}");
+            
+                let graph_data = GraphData {
+                    out_file: &out_file,
+                    title: &title,
+                    y_max_value: Some(optimal as f32 * 1.2),
+                    optimal_value_line: Some(optimal as f32),
+                    ..Default::default()
+                };
+            
+                run_and_plot_genetic_algorithm(&data, &graph_data, &mut n_optimal_results);
+            }
+            // println!();
+            let accuracy = n_optimal_results as f32 / n_reps as f32;
+            accuracy_table = format!("{accuracy_table} {} ", accuracy);
+        }
+        accuracy_table = format!("{accuracy_table}\n");
+    }
+    println!("{accuracy_table}");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,16 @@
 use anyhow::Error;
 use num_traits::Num;
 use plotters::prelude::*;
-use std::cmp::min;
 use std::convert::From;
 
 use crate::genetic_algorithm::GeneticAlgorithmResultData;
+
+const DEFAULT_FONT: &str = "sans-serif";
+const DEFAULT_FONT_SIZE: i32 = 30;
+const X_DESC: &str = "Generation";
+const Y_DESC: &str = "Score";
+const LEGEND_LABEL: &str = "Score for generation";
+const OPTIMAL_LEGEND_LABEL: &str = "Optimal score";
 
 pub struct GraphData<'a> {
     pub out_file: &'a str,
@@ -13,6 +19,8 @@ pub struct GraphData<'a> {
     pub margin: u32,
     pub x_label_area_size: u32,
     pub y_label_area_size: u32,
+    pub y_max_value: Option<f32>,
+    pub optimal_value_line: Option<f32>,
 }
 
 impl<'a> Default for GraphData<'a> {
@@ -23,7 +31,9 @@ impl<'a> Default for GraphData<'a> {
             title: "",
             margin: 5,
             x_label_area_size: 30,
-            y_label_area_size: 30,
+            y_label_area_size: 50,
+            y_max_value: None,
+            optimal_value_line: None,
         }
     }
 }
@@ -36,35 +46,67 @@ where
     T: Num + Clone + Ord + Default,
     f64: From<T>,
 {
-    let min_y = min(
-        T::default(),
-        data.score_per_generation.iter().min().unwrap().clone(),
+    let min_y = f32::min(
+        0.0,
+        to_f32(data.score_per_generation.iter().min().unwrap().clone()),
     );
-    let max_y = data.score_per_generation.iter().max().unwrap().clone();
+    let max_y = graph_data
+        .y_max_value
+        .unwrap_or(1.2 * to_f32(data.score_per_generation.iter().max().unwrap().clone()));
 
     let root = BitMapBackend::new(&graph_data.out_file, graph_data.graph_size).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
-        .caption(graph_data.title, ("sans-serif", 50).into_font())
+        .caption(
+            graph_data.title,
+            (DEFAULT_FONT, DEFAULT_FONT_SIZE).into_font(),
+        )
         .margin(graph_data.margin)
         .x_label_area_size(graph_data.x_label_area_size)
         .y_label_area_size(graph_data.y_label_area_size)
-        .build_cartesian_2d(
-            0.0f32..data.score_per_generation.len() as f32,
-            f64::from(min_y) as f32..(f64::from(max_y) * 1.2) as f32,
-        )?;
+        .build_cartesian_2d(0.0f32..data.score_per_generation.len() as f32, min_y..max_y)?;
 
-    chart.configure_mesh().draw()?;
+    chart
+        .configure_mesh()
+        .x_desc(X_DESC)
+        .y_desc(Y_DESC)
+        .draw()?;
 
-    chart.draw_series(LineSeries::new(
-        data.score_per_generation
-            .iter()
-            .enumerate()
-            .map(|(x, y)| (x as f32, f64::from((*y).clone()) as f32)),
-        &RED,
-    ))?;
+    if let Some(optimal_score) = graph_data.optimal_value_line {
+        chart
+            .draw_series(LineSeries::new(
+                (0..data.score_per_generation.len()).map(|x| (x as f32, optimal_score)),
+                &GREEN,
+            ))?
+            .label(OPTIMAL_LEGEND_LABEL)
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &GREEN));
+    }
+
+    chart
+        .draw_series(LineSeries::new(
+            data.score_per_generation
+                .iter()
+                .enumerate()
+                .map(|(x, y)| (x as f32, to_f32((*y).clone()))),
+            &RED,
+        ))?
+        .label(LEGEND_LABEL)
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
 
     root.present()?;
 
     Ok(())
+}
+
+fn to_f32<T>(value: T) -> f32
+where
+    f64: From<T>,
+{
+    f64::from(value) as f32
 }
