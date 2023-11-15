30994e4a6eeb519ac3ee7074f66fcdd8818f19fc
KacperSynator <kacper.synator@gmail.com>
2023-11-15 22:46:52 +0100

    add inversion and swap mutation methods

--- src/mutation_method.rs
+++ src/mutation_method.rs
@@ -4,6 +4,8 @@ use crate::individual::Individual;
 
 pub enum MutationMethod {
     BitFlip,
+    Swap,
+    Inversion,
 }
 
 pub fn mutation_method<T>(
@@ -16,6 +18,8 @@ where
 {
     match method {
         MutationMethod::BitFlip => bit_flip_mutation(individual, mutation_rate),
+        MutationMethod::Swap => swap_mutation(individual, mutation_rate),
+        MutationMethod::Inversion => inversion_mutation(individual, mutation_rate),
     }
 }
 
@@ -30,6 +34,44 @@ where
             *gene = !*gene;
         }
     });
+
+    new_individual
+}
+
+fn swap_mutation<T>(individual: &Individual<T>, mutation_rate: f64) -> Individual<T>
+where
+    T: Clone,
+{
+    let mut rng = rand::thread_rng();
+    let mut new_individual = individual.clone();
+
+    if !rng.gen_bool(mutation_rate) {
+        return new_individual;
+    }
+
+    let chromosome_len = new_individual.chromosome.len();
+    let (i,j) = (rng.gen_range(0..chromosome_len), rng.gen_range(0..chromosome_len));
+    new_individual.chromosome.swap(i, j);
+
+    new_individual
+}
+
+fn inversion_mutation<T>(individual: &Individual<T>, mutation_rate: f64) -> Individual<T>
+where
+    T: Clone,
+{
+    let mut rng = rand::thread_rng();
+    let mut new_individual = individual.clone();
+
+    if !rng.gen_bool(mutation_rate) {
+        return new_individual;
+    }
+
+    let chromosome_len = new_individual.chromosome.len();
+    let begin = rng.gen_range(0..chromosome_len);
+    let end = rng.gen_range(begin..chromosome_len);
+
+    new_individual.chromosome[begin..=end].reverse();
 
     new_individual
 }
